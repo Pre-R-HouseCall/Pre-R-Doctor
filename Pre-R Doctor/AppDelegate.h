3e31e9a410ff552ed7b9e27e7d0553df8510f333
timacorda <tacorda@calpoly.edu>
2015-05-30 22:42:59 -0700

    Committing to files

---
+++ Pre-R Doctor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pre-R Doctor
+//
+//  Created by Timothy Acorda on 5/14/15.
+//  Copyright (c) 2015 Pre-R. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
