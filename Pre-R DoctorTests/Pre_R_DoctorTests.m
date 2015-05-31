bd991a4dae3bbfd6142300240a85b96e680b3b15
timacorda <tacorda@calpoly.edu>
2015-05-30 22:44:25 -0700

    kk

---
+++ Pre-R DoctorTests/Pre_R_DoctorTests.m
@@ -0,0 +1,40 @@
+//
+//  Pre_R_DoctorTests.m
+//  Pre-R DoctorTests
+//
+//  Created by Timothy Acorda on 5/14/15.
+//  Copyright (c) 2015 Pre-R. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Pre_R_DoctorTests : XCTestCase
+
+@end
+
+@implementation Pre_R_DoctorTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
